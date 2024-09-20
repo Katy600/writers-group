dcb9dbda77cb8a4a55de7377b8bf1209a0008dde
Katy McCann <katherine.mccann@digital.justice.gov.uk>
2024-09-20 14:25:13 +0100

    Clean up code

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,22 +1,2 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
-
-const navMenu = document.getElementById("nav-menu"),
-    navToggle = document.getElementById("nav-toggle"),
-    navClose = document.getElementById("nav-close");
-
-/* Menu show */
-if (navToggle) {
-    navToggle.addEventListener("click", () => {
-        navMenu.classList.add("show-menu");
-    });
-}
-
-/* Menu hidden */
-if (navClose) {
-    navClose.addEventListener("click", () => {
-        navMenu.classList.remove("show-menu");
-    });
-}
 
